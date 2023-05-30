cb04d2cd0012d222ffa2e7beb3b033a2c4121ee7
Jose Uribe <joseuribeh@gmail.com>
2023-05-29 22:25:27 -0500

    Debugger in JS

---
+++ src/debug/index.js
@@ -0,0 +1,9 @@
+var a = 'Hello'
+
+function hello() {
+    let b = 'Hello World'
+    const c = 'Hello World!'
+    debugger //Pausa la ejecucion del codigo. Usando devTools de Chrome podemos ver muchos mas elementos que nos van a servir para analizar y debuggear nuestro codigo
+}
+
+hello()
