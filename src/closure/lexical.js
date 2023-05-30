5374133503a5d385460b04f69d47e47fa1fa0b3f
Jose Uribe <joseuribeh@gmail.com>
2023-05-29 21:34:15 -0500

    Lexical Closure exercise

--- src/closure/lexical.js
+++ src/closure/lexical.js
@@ -29,3 +29,26 @@ myFunction()
 
 //Todas las variables se muestran correctamente, aunque esten anidadas dentro de otras funciones
 //En el ambito lexico la accesibilidad de las variables esta determinada por la posicion de las mismas dentro de los ambitos anidados
+
+//En este desafío recibirás dos números aleatorios por parámetros.
+//Usando las closures deberás crear una función a la cual se pueda llamar en primer lugar con un solo número y secuencialmente volver a llamarla para completar la suma con el siguiente número:
+//La solución deberá tener un input y output como la siguiente, en algunos casos puede que no se mande el segundo número por lo que deberás pensar en como manejar ese tipo de casos.
+//Input:
+//sumWithClosure(2)(3);
+//Output:
+//5
+//Input:
+//sumWithClosure(90)();
+//Output:
+//90
+
+//Solucion
+
+function sumWithClosure(firstNum = 0) {
+    return function (secondNum = 0) {
+        return firstNum + secondNum
+    }
+}
+
+sumWithClosure(2)(3)
+sumWithClosure(90)()
